0df267ac6c5a7a0e1d9c7bc0f0ba9cd6bfb611db
Cogan Shimizu <shimizu.5@wright.edu>
2020-02-12 19:51:13 -0600

    added a subject class for convenience in operating over subjects, as well as the ability to add subjects directly to the current_experiment

--- core.js
+++ core.js
@@ -1,9 +1,36 @@
 class Experiment {
-    constructor(experiment_data, subjects_data, scoring_data) {
+    constructor(experiment_data, subjects_data, behaviour_parameters) {
         this.experiment_data = experiment_data;
         this.subjects_data = subjects_data;
-        this.scoring_data = scoring_data;
+        this.behaviour_parameters = behaviour_parameters;
     };
+
+    addSubject(subject) {
+        this.subjects_data.push(subject);
+    }
+}
+
+class Subject {
+    constructor()
+    {
+        this.subject_id = "";
+        this.genotype = "";
+        this.sex = "";
+        this.group = "";
+        this.treatment = "";
+        this.comment = "";
+        this.scoring_data = [];
+    }
+
+    setFields(fieldValues)
+    {
+        this.subject_id = fieldValues[0];
+        this.genotype = fieldValues[1];
+        this.sex = fieldValues[2];
+        this.group = fieldValues[3];
+        this.treatment = fieldValues[4];
+        this.comment = fieldValues[5];
+    }
 }
 
 let experiment_data = {name: "Crags", date:"02/09/2020"};
@@ -26,9 +53,18 @@ let subjects_data = [
     }
 ]
 
-let scoring_data = {};
+let behaviour_parameters = [
+    {
+        "key": "A",
+        "behaviour": "Grooming",
+    },
+    {
+        "key": "2",
+        "behaviour": "Dining",
+    }
+];
 
-let current_experiment = new Experiment(experiment_data,subjects_data,scoring_data);
+let current_experiment = new Experiment(experiment_data,subjects_data,behaviour_parameters);
 
 console.log(JSON.stringify(current_experiment));
 
